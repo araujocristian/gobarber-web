2a7aabc8a0685edf0818ea0e6207c773b17abb3b
Cristian Araujo <araujocristian@live.com>
2020-07-07 20:37:57 -0300

    Configurando rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
